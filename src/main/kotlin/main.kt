7b7d4e7ce69bc576fe48fa814f95c1ee4444073f
Jeff Cadieux <jcadieux@d155.org>
2021-05-20 11:14:10 -0500

    Initial commit

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,8 @@
+ fun main(){
+     println("I have been teaching for 15 years at CLS")
+     println("I know Java and JavaScript, but")
+     println("I have been learning and teaching low level")
+     println("programming for years and to honest")
+     println("very anxious about moving beyond that low level")
+
+ }
